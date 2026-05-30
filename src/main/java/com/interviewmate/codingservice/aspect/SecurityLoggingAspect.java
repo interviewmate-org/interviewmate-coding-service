cad661f35ff6e2c565a4e8ab4d946cd1d9241079
MrPal28 <arindampal669@gmail.com>
2026-05-30 16:57:36 +0530

    reffining the file name

--- src/main/java/com/interviewmate/codingservice/aspect/SecurityLoggingAspect.java
+++ src/main/java/com/interviewmate/codingservice/aspect/SecurityLoggingAspect.java
@@ -1,4 +1,4 @@
-package com.interviewmate.codingservice.aop;
+package com.interviewmate.codingservice.aspect;
 
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
